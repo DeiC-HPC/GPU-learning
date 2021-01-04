1cb16c5171438a9e3115c85a40176ab1d6fd5191
Tethys Svensson <freaken@freaken.dk>
2021-01-04 13:49:37 +0100

    dev.sh now generates jupyter notebooks

--- shell.nix
+++ shell.nix
@@ -14,6 +14,37 @@ pkgs.mkShell {
       (cd mdbook; exec mdbook serve &>/dev/null </dev/null) &
       MDBOOK_PID=$!
 
+      rm -rf notebooks
+      cp -r examples notebooks
+      for f in $(find notebooks -type f ); do
+        ext="''${f##*.}"
+        typ=""
+        case $ext in
+          py) typ=python;;
+          cpp)
+            if echo $f | grep -q openacc; then
+              typ=cpp_openacc
+            else
+              typ=cpp_openmp
+            fi
+          ;;
+          f90)
+            if echo $f | grep -q openacc; then
+              typ=fortran_openacc
+            else
+              typ=fortran_openmp
+            fi
+          ;;
+          cu) typ=cuda;;
+        esac
+        if [[ -n "$typ" ]]; then
+          (cd source-to-notebook && cargo run $typ ../$f ../''${f%%.*}.ipynb)
+          rm -f $f
+        else
+          echo UNKNOWN $f
+        fi
+      done
+
       JUPYTER_CONFIG=$PWD/jupyter-config.py
       nix build ../hpc-nix#jupyter -o result-jupyter
       export JUPYTER_HEADER_FILES=$PWD/include
